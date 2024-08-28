01a3f3c8c741bb48488323e82130f393048fc3f2
urf94 <urf@mercuryproject.com>
2024-08-28 11:12:33 +0900

    fix checkpoint path

--- changepoint_detection/inference.py
+++ changepoint_detection/inference.py
@@ -1,17 +1,14 @@
 import os
-import warnings
 
 from pytorch_lightning import Trainer
 
 from .utils import load_model
 
-warnings.filterwarnings("ignore", category=FutureWarning, module="torch")
 
 def inference_prophet(df, scale, checkpoint_dir=None):
     # 패키지 내 checkpoint 폴더를 기본값으로 설정
     if checkpoint_dir is None:
-        checkpoint_dir = os.path.join(os.path.dirname(__file__), "checkpoint")
-
+        checkpoint_dir = os.path.join(os.path.dirname(__file__), "..", "checkpoint")
     model_filename = f"prophet_scale_{scale}.pkl"
     model_path = os.path.join(checkpoint_dir, model_filename)
 
